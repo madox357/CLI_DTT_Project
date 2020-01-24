2cb3cee45560920146c72704d11a2967c3843bb3
Bijaya Limbu <bl18aat@herts.ac.uk>
2020-01-24 12:14:15 +0000

    webpack error fix by tweaking file path in src/asset/

---
+++ src/assets/App.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <div>Hello</div>
+  <div> {{name}} </div>
+</div>
+</template>
+
+<script>
+  export default {
+      data(){
+        return {
+          name: "Bijaya"
+
+        }
+
+
+      }
+
+}
+</script>
+
+<style >
+
+</style>
